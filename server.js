8c94026583decfb31b135ca9e576ad34a9ec1e54
FTA7700 <61029883+FTA7700@users.noreply.github.com>
2026-03-16 17:07:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("running");
+  res.send("Keepalive server running");
 });
 
 app.listen(PORT, () => {
